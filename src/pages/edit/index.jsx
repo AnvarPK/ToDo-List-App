d1c0751321372b545d0ab29a8a3134c9d53edeae
Anvar pk <anvar.pk@quest-global.com>
2021-11-28 01:36:24 +0530

    Edit page

--- src/pages/edit/index.jsx
+++ src/pages/edit/index.jsx
@@ -1,9 +1,19 @@
+import { Grid, Typography } from "@mui/material"
+import ToDoForm from "../../components/todo-form"
 
 const EditToDo = () => {
+    const handleSubmit = data => {
+        console.log(data)
+    }
     return (
-        <div>
-            
-        </div>
+        <Grid container spacing={2} >
+            <Grid item xs={12}>
+                <Typography variant="h6" >Edit To Do</Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <ToDoForm handleSubmit={handleSubmit} />
+            </Grid>
+        </Grid>
     )
 }
 
