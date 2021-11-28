1ad967e6f56a270f7478499b678b70fb54c26e16
Anvar pk <anvar.pk@quest-global.com>
2021-11-28 09:09:00 +0530

    Dete Fix

--- src/helper/setLocal.js
+++ src/helper/setLocal.js
@@ -2,10 +2,7 @@ import { APP_CONSTANTS } from "../appconsts";
 
 export const setLocalTodos = (todos = []) => {
     try {
-        if (todos.length > 0) {
-            todos = JSON.parse(localStorage.setItem(APP_CONSTANTS.TODO_LOCAL_STORAGE_KEY, JSON.stringify(todos)));
-        }
-
+        todos = JSON.parse(localStorage.setItem(APP_CONSTANTS.TODO_LOCAL_STORAGE_KEY, JSON.stringify(todos)));
     }
     catch (e) {
         console.error(e);
